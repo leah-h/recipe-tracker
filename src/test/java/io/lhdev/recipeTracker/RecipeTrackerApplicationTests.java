cc29ea7be4edddec18b91910a283a473bf2ecb43
Leah H <harrichl@gmail.com>
2021-11-28 21:47:06 -0500

    initial project setup

---
+++ src/test/java/io/lhdev/recipeTracker/RecipeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lhdev.recipeTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
